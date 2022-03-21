ab1270c243fa653dde16f7b39a87126fb0219fb1
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-22 02:09:24 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Make e-commerce website</h1>
+    <div >
+      <Header></Header>
     </div>
   );
 }
