e79f11a8ad829a490b1dbe60a54c37e47b16e0d0
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-31 02:29:55 +0600

    add route this project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Component/Header/Header';
+import Inventory from './Component/Inventory/Inventory';
+import Orders from './Component/Orders/Orders';
 import Shop from './Component/Shop/Shop';
 
 function App() {
   return (
     <div >
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path='/' element={<Shop/>}></Route>
+        <Route path='/shop' element={<Shop/>}></Route>
+        <Route path='/orders' element={<Orders/>}></Route>
+        <Route path='/inventory' element={<Inventory/>}></Route>
+      </Routes>
+      
     </div>
   );
 }
