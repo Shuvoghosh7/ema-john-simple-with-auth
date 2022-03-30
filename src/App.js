018de2e316ef241d13e354a76f2101a157a7b3a6
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-31 03:49:00 +0600

    add custom function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Component/About/About';
 import Header from './Component/Header/Header';
 import Inventory from './Component/Inventory/Inventory';
 import Orders from './Component/Orders/Orders';
@@ -14,6 +15,7 @@ function App() {
         <Route path='/shop' element={<Shop/>}></Route>
         <Route path='/orders' element={<Orders/>}></Route>
         <Route path='/inventory' element={<Inventory/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
       </Routes>
       
     </div>
--- src/Component/Orders/Orders.js
+++ src/Component/Orders/Orders.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useProducts from '../../Hooks/useProduct';
+
+
+
 
 const Orders = () => {
+    const[product,setProduct]=useProducts()
     return (
         <div>
-            <h1>Order Now</h1>
+            <h1>Order Now:{product.length}</h1>
         </div>
     );
 };
